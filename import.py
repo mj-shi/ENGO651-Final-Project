41cabe7d72b62e607340be98fac200e9263cfeb0
Michael <michael.shi@ucalgary.ca>
2022-04-07 04:51:15 -0600

    Update import.py

--- import.py
+++ import.py
@@ -4,9 +4,9 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-# engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv("DATABASE_URL"))
 # Manually set up DATABASE_URL
-engine = create_engine("postgresql://vogkflkqcyrnzs:985bbfead7f2ec0711a0c8b20eba318d7fdf5b16418f4f5a915f1bfbc642f845@ec2-54-160-109-68.compute-1.amazonaws.com:5432/d3smrh8696l9sm")
+# engine = create_engine("postgresql://vogkflkqcyrnzs:985bbfead7f2ec0711a0c8b20eba318d7fdf5b16418f4f5a915f1bfbc642f845@ec2-54-160-109-68.compute-1.amazonaws.com:5432/d3smrh8696l9sm")
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
